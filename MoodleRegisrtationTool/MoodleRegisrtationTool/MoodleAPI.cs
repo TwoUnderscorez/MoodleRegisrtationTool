6062aa5e13e859f2bac8932f31d50f14037d5539
TwoUnderscorez <ron.yutkin@gmail.com>
2018-08-06 00:16:24 +0300

    Start implementing UploadUsers

--- MoodleRegisrtationTool/MoodleRegisrtationTool/MoodleAPI.cs
+++ MoodleRegisrtationTool/MoodleRegisrtationTool/MoodleAPI.cs
@@ -10,14 +10,37 @@ namespace MoodleRegisrtationTool
 {
     class MoodleAPI
     {
+        #region Private Variables
         ScriptRuntime python;
         dynamic moodle;
         dynamic mdl;
+        #endregion
+
+        #region Constructor
         public MoodleAPI()
         {
             python = Python.CreateRuntime();
             moodle = python.ImportModule("moodle.moodle");
             mdl = moodle.MDL();
         }
+        #endregion
+
+        #region Methods
+
+        public async Task<IDictionary<string,string>> UploadUsers(IDictionary<string, string> Server, IList<IDictionary<string, string>> Users)
+        {
+            string result = await Task.Run(() =>
+            {
+                /* Decide which protocol function to use from the moodle api and
+                 * execute the function with the function parameters.
+                 */
+                dynamic ProtocolFunction = (Server["protocol"] == "rest") ? mdl.rest_protocol : mdl.xmlrpc_protocol;
+                return (string)ProtocolFunction(Server, Users, "core_user_create_users", "users");
+            });
+            /* Parse moodle's xml response into a nice dictionary to update the GUI with. */
+            return null;
+        }
+
+        #endregion
     }
 }
