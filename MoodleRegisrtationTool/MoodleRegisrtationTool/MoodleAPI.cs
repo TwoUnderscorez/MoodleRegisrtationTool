696db4c5662ae2a3418a94feadb22645c1554cbf
TwoUnderscorez <ron.yutkin@gmail.com>
2018-08-05 22:23:09 +0300

    Add MoodleAPI class for communication with the moodle webservice

---
+++ MoodleRegisrtationTool/MoodleRegisrtationTool/MoodleAPI.cs
@@ -0,0 +1,23 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodleRegisrtationTool
+{
+    class MoodleAPI
+    {
+        ScriptRuntime python;
+        dynamic moodle;
+        dynamic mdl;
+        public MoodleAPI()
+        {
+            python = Python.CreateRuntime();
+            moodle = python.ImportModule("moodle.moodle");
+            mdl = moodle.MDL();
+        }
+    }
+}
